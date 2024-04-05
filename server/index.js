d00b63ff2b4647bc4b04d108e5bb0239c9067fb2
Kurt Euraj <kurteurajdev@gmail.com>
2024-04-04 23:45:39 -0400

    Add orders routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const orderRoutes = require('./routes/orders-route');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.json())
+app.use(cors())
+
+
+
+
+app.use('/api/orders', orderRoutes);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
