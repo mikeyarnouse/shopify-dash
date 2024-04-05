fac2a7ad000cbbe25eb909a23d6666dd67fd2f53
KAMERON SEABROOK <kameronseabrook@KAMERONs-MacBook-Pro.local>
2024-04-05 12:41:08 -0400

    header-partially completed

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer'
+
+function Footer(){
+
+    return (
+        <>
+        <section> Footer
+        
+        
+        </section>
+        </>
+    )
+}
+
+export default Footer
