f6177e42f68108bbdca62962a8729023062ca5b5
michaelarnouse <michael.arnouse@richmond.edu>
2024-04-04 23:37:21 -0400

    finished profile routes

--- server/index.js
+++ server/index.js
@@ -5,5 +5,12 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use(cors());
+
+const profileRoute = require("./routes/profile-route");
+
+app.use("/api/profile", profileRoute);
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- server/routes/profile-route.js
+++ server/routes/profile-route.js
@@ -1,1 +1,29 @@
 const router = require("express").Router();
+const knex = require("knex")(require("../knexfile"));
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const userProfile = await knex("profile").select("*");
+      res.json(userProfile[0]);
+    } catch (e) {
+      res.status(500).json({
+        message: `Unable to retrieve profile data. ${e}`,
+      });
+    }
+  })
+  .patch(async (req, res) => {
+    try {
+      await knex("profile").update(req.body);
+
+      const updatedProfile = await knex("profile").select("*");
+      res.json(updatedProfile[0]);
+    } catch (e) {
+      res.status(500).json({
+        message: `Unable to update profile data. ${e}`,
+      });
+    }
+  });
+
+module.exports = router;
